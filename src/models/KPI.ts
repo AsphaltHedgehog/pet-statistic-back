31e099cf84ad18523b58d467a60ac2f975f2919e
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-10 22:53:26 +0300

    started working on Models

--- src/models/KPI.ts
+++ src/models/KPI.ts
@@ -1,11 +1,88 @@
+// const mongooseCurrency = require("mongoose-currency");
 import mongoose from "mongoose";
-import { loadType } from "mongoose-currency";
-
 const Schema = mongoose.Schema;
 
-interface IKPISchema {}
+// interface IKPISchema {
+//   totalProfit: loadType.number
+//   totalRevenue: loadType.number
+//   totalExpenses: loadType.number
+//   expensesByCategory: string:loadType.number[]
+// }
 
-const KPISchema = new Schema();
+const mouthSchema = new Schema(
+	{
+		mouth: String,
+		revenue: {
+			type: mongoose.Types.Decimal128,
+			currency: "USD",
+			get: (v: number) => v / 100,
+		},
+		expenses: {
+			type: mongoose.Types.Decimal128,
+			currency: "USD",
+			get: (v: number) => v / 100,
+		},
+		operationalExpenses: {
+			type: mongoose.Types.Decimal128,
+			currency: "USD",
+			get: (v: number) => v / 100,
+		},
+		nonOperationalExpenses: {
+			type: mongoose.Types.Decimal128,
+			currency: "USD",
+			get: (v: number) => v / 100,
+		},
+	},
+	{ toJSON: { getters: true } }
+);
+
+const daySchema = new Schema(
+	{
+		date: String,
+		revenue: {
+			type: mongoose.Types.Decimal128,
+			currency: "USD",
+			get: (v: number) => v / 100,
+		},
+		expenses: {
+			type: mongoose.Types.Decimal128,
+			currency: "USD",
+			get: (v: number) => v / 100,
+		},
+	},
+	{ toJSON: { getters: true } }
+);
+
+const KPISchema = new Schema(
+	{
+		totalProfit: {
+			type: mongoose.Types.Decimal128,
+			currency: "USD",
+			get: (v: number) => v / 100,
+		},
+		totalRevenue: {
+			type: mongoose.Types.Decimal128,
+			currency: "USD",
+			get: (v: number) => v / 100,
+		},
+		totalExpenses: {
+			type: mongoose.Types.Decimal128,
+			currency: "USD",
+			get: (v: number) => v / 100,
+		},
+		expensesByCategory: {
+			type: Map,
+			of: {
+				type: mongoose.Types.Decimal128,
+				currency: "USD",
+				get: (v: number) => v / 100,
+			},
+		},
+		mouthSchema: [mouthSchema],
+		dailyData: [daySchema],
+	},
+	{ toJSON: { getters: true }, timestamps: true }
+);
 
 const KPI = mongoose.model("KPI", KPISchema);
 
