054cdf8337406ac799018e831a4779d58d69eb53
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-10 22:54:15 +0300

    Update kpiRoutes.ts

--- src/routes/kpiRoutes.ts
+++ src/routes/kpiRoutes.ts
@@ -6,6 +6,8 @@ const router = express.Router();
 router.get("/kpis", async (req, res) => {
 	try {
 		const kpis = await KPI.find();
+
+		res.status(200).json({ kpis });
 	} catch (error: any) {
 		res.status(404).json({ message: error.message });
 	}
