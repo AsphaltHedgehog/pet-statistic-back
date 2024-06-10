4d52c1f2f1322d7b06e9660025c5dc22a57a08ed
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-06-11 01:49:19 +0300

    fixed response
    change object response to array

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -4,12 +4,12 @@ import Product from "../models/Product";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-	try {
-		const products = await Product.find();
-		res.status(200).json({ products });
-	} catch (error: any) {
-		res.status(404).json({ message: error.message });
-	}
+  try {
+    const products = await Product.find();
+    res.status(200).json([...products]);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
 export default router;
